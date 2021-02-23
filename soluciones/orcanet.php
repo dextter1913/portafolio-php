3200afec00e41cae87eb1b59fb9633ea51a3b1fa
dextter1913 <Clau32usa@gmai.com>
2021-02-23 18:45:25 -0500

    Actualizacion

--- soluciones/orcanet.php
+++ soluciones/orcanet.php
@@ -38,10 +38,23 @@
         
     <div class="cuerpo">
         <h1>Conoce Orca 1.0</h1><br><br>
-        <img src="../imagen/Orca/Captura.PNG" alt="" class="imagenes">
-        <p>Orca inicia con 2 botones, el de empleado y el de Administrador, el sgundo lo presentaremos mas adeante, por ahora soo veremos e primero, continuemos...</p>
-        <img src="../imagen/Orca/Captura1.PNG" alt="" class="imagenes">
-        <p>Aquí ingresaran las credenciales del empleado, tanto usuario y contraseña suministrado por el area administrativa o por su jefe inmediato...</p>
+        <img src="../imagen/Orca/Captura.PNG" alt="" class="imagenes"><br>
+        <p>Orca inicia con 2 botones, el de empleado y el de Administrador, el sgundo lo presentaremos mas adeante, por ahora soo veremos e primero, continuemos...</p><br><br>
+        <img src="../imagen/Orca/Captura1.PNG" alt="" class="imagenes"><br>
+        <p>Aquí ingresaran las credenciales del empleado, tanto usuario y contraseña suministrado por el area administrativa o por su jefe inmediato...</p><br><br>
+        <img src="../imagen/Orca/Captura2.PNG" alt="" class="imagenes"><br>
+        <p>te saludará la pantalla principal, la cual reconoceras porque en la parte superior tiene siertas opciones, que son: <b>Buscar Ciente, Registrar y Acerca de</b>, ya hablaremos mas a fondo de cada una de estas opciones...</p><br><br>
+        <img src="../imagen/Orca/Captura3.PNG" alt="" class="imagenes"><br>
+        <p>Opcion <b>Buscar Cliente</b> aca podras buscar a tu cliente ya registrado, con su dicho criterio, ya sea con cedula o ID previamente generado en la base de datos, si no recuerda ninguno de estos datos, en la parte inferior, podra tambien ingresar <b>Nombre</b>, <b>Apellido</b>, y/o <b>Telefono</b> para posteriormente encontrar el ID o cedula y así ingresar su pedido</p><br><br>
+        <img src="../imagen/Orca/Captura4.PNG" alt="" class="imagenes"><br>
+        <p>una pequeña demostracion de como filtrar por apellido para buscar al cliente</p><br><br>
+        <img src="../imagen/Orca/Captura5.PNG" alt="" class="imagenes"><br>
+        <p>Despues de encontrar al Cliente, Orca ya te permitira ingresar el pedido del ciente dando click en 
+        <b>Ingresar Pedido</b></p><br><br>
+        <img src="../imagen/Orca/Captura6.PNG" alt="" class="imagenes"><br>
+        
+
+        
     </div>
     <footer style="background-color: #2c3e50; color:#dfe4ea; font-size: 20px; font-family:'Times New Roman', Times, serif; text-align: right;">Copyright © Cristian Aguirre</footer>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
