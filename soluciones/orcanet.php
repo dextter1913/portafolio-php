a540f4b5d12e192c6b3da9f195d5363bf8cafadf
dextter1913 <Clau32usa@gmai.com>
2021-02-23 19:37:44 -0500

    Actualizacion

--- soluciones/orcanet.php
+++ soluciones/orcanet.php
@@ -38,21 +38,48 @@
         
     <div class="cuerpo">
         <h1>Conoce Orca 1.0</h1><br><br>
-        <img src="../imagen/Orca/Captura.PNG" alt="" class="imagenes"><br>
-        <p>Orca inicia con 2 botones, el de empleado y el de Administrador, el sgundo lo presentaremos mas adeante, por ahora soo veremos e primero, continuemos...</p><br><br>
-        <img src="../imagen/Orca/Captura1.PNG" alt="" class="imagenes"><br>
-        <p>Aquí ingresaran las credenciales del empleado, tanto usuario y contraseña suministrado por el area administrativa o por su jefe inmediato...</p><br><br>
-        <img src="../imagen/Orca/Captura2.PNG" alt="" class="imagenes"><br>
-        <p>te saludará la pantalla principal, la cual reconoceras porque en la parte superior tiene siertas opciones, que son: <b>Buscar Ciente, Registrar y Acerca de</b>, ya hablaremos mas a fondo de cada una de estas opciones...</p><br><br>
-        <img src="../imagen/Orca/Captura3.PNG" alt="" class="imagenes"><br>
-        <p>Opcion <b>Buscar Cliente</b> aca podras buscar a tu cliente ya registrado, con su dicho criterio, ya sea con cedula o ID previamente generado en la base de datos, si no recuerda ninguno de estos datos, en la parte inferior, podra tambien ingresar <b>Nombre</b>, <b>Apellido</b>, y/o <b>Telefono</b> para posteriormente encontrar el ID o cedula y así ingresar su pedido</p><br><br>
-        <img src="../imagen/Orca/Captura4.PNG" alt="" class="imagenes"><br>
-        <p>una pequeña demostracion de como filtrar por apellido para buscar al cliente</p><br><br>
-        <img src="../imagen/Orca/Captura5.PNG" alt="" class="imagenes"><br>
+        <p>Orca inicia con 2 botones, el de empleado y el de Administrador, el sgundo lo presentaremos mas adeante, por ahora soo veremos e primero, continuemos...</p><br>
+        <img src="../imagen/Orca/Captura.PNG" alt="" class="imagenes"><br><br><br>
+        <p>Aquí ingresaran las credenciales del empleado, tanto usuario y contraseña suministrado por el area administrativa o por su jefe inmediato...</p><br>
+        <img src="../imagen/Orca/Captura1.PNG" alt="" class="imagenes"><br><br><br>
+        <p>te saludará la pantalla principal, la cual reconoceras porque en la parte superior tiene siertas opciones, que son: <b>Buscar Ciente, Registrar y Acerca de</b>, ya hablaremos mas a fondo de cada una de estas opciones...</p><br>
+        <img src="../imagen/Orca/Captura2.PNG" alt="" class="imagenes"><br><br><br>
+        <p>Opcion <b>Buscar Cliente</b> aca podras buscar a tu cliente ya registrado, con su dicho criterio, ya sea con cedula o ID previamente generado en la base de datos, si no recuerda ninguno de estos datos, en la parte inferior, podra tambien ingresar <b>Nombre</b>, <b>Apellido</b>, y/o <b>Telefono</b> para posteriormente encontrar el ID o cedula y así ingresar su pedido...</p><br>
+        <img src="../imagen/Orca/Captura3.PNG" alt="" class="imagenes"><br><br><br>
+        <p>una pequeña demostracion de como filtrar por apellido para buscar al cliente...</p><br>
         <p>Despues de encontrar al Cliente, Orca ya te permitira ingresar el pedido del ciente dando click en 
-        <b>Ingresar Pedido</b></p><br><br>
-        <img src="../imagen/Orca/Captura6.PNG" alt="" class="imagenes"><br>
-        
+        <b>Ingresar Pedido</b>...</p><br>
+        <img src="../imagen/Orca/Captura4.PNG" alt="" class="imagenes"><br><br><br>
+        <img src="../imagen/Orca/Captura5.PNG" alt="" class="imagenes"><br><br><br>
+        <p>podras ingresar el codigo del producto, posteriormente le debes dar en <b>Buscar</b>, el sistema buscara os productos ingresados 
+            a la base de datos, con su respectivo precio y el ID de su correspondiente categoria, en este caso, <b>Verduras</b>,
+            tambien puedes ingresar la cantidad de productos que llevara el ciente, despues le das click en <b>Facturar</b>, posterior, puedes
+            buscar otro producto y repites el mismo proceso cuantos pedidos tengas en el momento
+        </p><br>
+        <img src="../imagen/Orca/Captura6.PNG" alt="" class="imagenes"><br><br><br>
+        <img src="../imagen/Orca/Captura7.PNG" alt="" class="imagenes"><br><br><br>
+        <p>Antes de ingresar la compra, debes si o si, ingresar un <b>numero de caja</b>, (Aplica para los supermercados) si el negocio
+            es otro tipo, por defecto puedes ingresar el numero 1 en dicha caja de texto, ya que no debe quedar en blanco
+        </p><br>
+        <img src="../imagen/Orca/Captura8.PNG" alt="" class="imagenes"><br><br><br>
+        <P>Despues de haberle dado click en <b>Ingresar Compra</b> podemos observar como se ingresa 
+        la compra a la base de datos, y así e sistema le dara un <b>codigo de compra</b>, ante cuaquier devolucion, el sistema te permitirá
+        Exportar en un Documento de Excel, antes debes crear un archivo xls llamado pagos en la ruta donde se instalo el <b>Orca</b>
+        sirve para revisar todas las ventas realizadas hasta el momento, con su respectiva fecha y hora de ingreso, de dicho Pedido 
+        </P><br>
+        <img src="../imagen/Orca/Captura9.PNG" alt="" class="imagenes"><br><br><br>
+        <p>Ejemplo de como se veria el anterior exporte</p><br>
+        <img src="../imagen/Orca/Captura10.PNG" alt="" height="600px" class="imagenes"><br><br><br>
+        <p>
+            Ahora pasamos a la siguiente opcion del menu llamada, <b>Registrar Producto</b>, a encontaras en la parte 
+            superior, pasamos el puntero en registrar, producto y ahí encontraremos a opcion <b>Registrar Producto</b>
+            evidenciaras, 3 campos especificos, <b>Nombre del Producto</b>, <b>Precio del Producto</b>, <b>ID Categoria</b>,
+            evidentemente, puedes ingresar el Nombre del producto a ingresar, precio del producto y el ID de la categoria Asígnada a producto,
+            si no sabes cua es el numero de ID de la categoria de tu negocio, Facíl, le das click en Mostrar categoria y así las veras todas
+        </p><br>
+        <img src="../imagen/Orca/Captura11.PNG" alt="" class="imagenes"><br><br><br>
+        <p></p><br><br>
+        <img src="../imagen/Orca/Captura12.PNG" alt="" class="imagenes"><br><br><br>
 
         
     </div>
